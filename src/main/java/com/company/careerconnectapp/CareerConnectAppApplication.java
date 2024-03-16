6e9f9cd8cc79b892f2974e9407e9ff2d9e0d5edf
SanjanaBhandure <sweetybhandure@gmail.com>
2024-03-16 16:28:50 +0530

    Initial commit

---
+++ src/main/java/com/company/careerconnectapp/CareerConnectAppApplication.java
@@ -0,0 +1,13 @@
+package com.company.careerconnectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerConnectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerConnectAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/careerconnectapp/CareerConnectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.careerconnectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerConnectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
